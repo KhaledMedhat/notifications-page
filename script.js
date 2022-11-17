c54c9767ae60980f6d3b0e4a03b59f9706389588
Khaled Medhat <96800949+KhaledMedhat@users.noreply.github.com>
2022-11-17 09:55:00 +0200

    Responsive and media query
    updating the html template to make better css for it and made it a responsive to all widths and for all devices

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-const notificationsContent = document.querySelectorAll('.noti-content')
-const points = document.querySelectorAll('.point')
-const button = document.querySelector('.read')
+const notificationsContainer = document.querySelectorAll(
+  '.notification-container'
+)
+const circles = document.querySelectorAll('.circle')
+const button = document.querySelector('.rightside')
 let count = document.querySelector('.counter')
-let number = points.length
+let number = circles.length
 
 let html = `${number}`
 count.innerHTML = html
 
-notificationsContent.forEach((content) => {
+notificationsContainer.forEach((content) => {
+  const seen = content.children[0].children[1].children.namedItem('ss')
   content.addEventListener(
     'click',
     (e) => {
       content.setAttribute('style', 'background-color:white ')
-      content.children[0].children[2].classList.remove('point')
+      seen.style.display = 'none'
       if (number !== 0) {
         html = `${number - 1}`
         number--
@@ -24,11 +27,11 @@ notificationsContent.forEach((content) => {
 })
 
 button.addEventListener('click', (e) => {
-  notificationsContent.forEach((content) => {
+  notificationsContainer.forEach((content) => {
     content.setAttribute('style', 'background-color:white ')
   })
-  points.forEach((point) => {
-    point.setAttribute('style', 'display:none')
+  circles.forEach((circle) => {
+    circle.setAttribute('style', 'display:none')
   })
   html = 0
   count.innerHTML = html
